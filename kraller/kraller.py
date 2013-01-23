3bf4f0d9763b3e0877d0c24f44d102e0fc848920
Josh Snyder <josh@code406.com>
2013-01-23 17:49:49 -0500

    CAS works, could be prettier

--- kraller/kraller.py
+++ kraller/kraller.py
@@ -4,15 +4,55 @@ Kraller
 An application to allow signups for accounts on a server with a key.
 """
 
-from flask import Flask, render_template
+from datetime import timedelta
+from functools import wraps
+from urllib import urlencode
+
+from flask import Flask, abort, render_template, redirect, request, session, url_for
+import requests
+
+from itsdangerous_session import ItsDangerousSessionInterface
 
 app = Flask(__name__)
+app.secret_key = 'hackme'
+app.session_interface = ItsDangerousSessionInterface()
+app.permanent_session_lifetime = timedelta(hours=2)
 
+cas_server_endpoint = 'https://login.case.edu/cas/'
+
+def my_cas_endpoint(redirect_to=None):
+    if redirect_to is None:
+        redirect_to = request.path
+    return url_for('login', redirect_to=redirect_to, _external=True)
+
+def requires_auth(f):
+    wraps(f)
+    def decorated(*args, **kwargs):
+        if 'username' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(cas_server_endpoint + 'login?' + urlencode(dict(service=my_cas_endpoint(), renew='true')))
+    return decorated
+
+@app.route('/login')
+def login():
+    if 'ticket' in request.args and 'redirect_to' in request.args:
+        ticket = request.args.get('ticket')
+        redirect_to = request.args.get('redirect_to')
+        r = requests.get(cas_server_endpoint + 'validate', params=dict(service=my_cas_endpoint(redirect_to), ticket=ticket), verify=True)
+        if not r.status_code == requests.codes.ok:
+            return abort(500)
+        (answer, username) = r.text.split('\n', 1)
+        if answer == 'yes':
+            #set cookie and redirect
+            session['username'] = username
+            return redirect(redirect_to)
+    return abort(401)
 
 @app.route('/')
+@requires_auth
 def index():
-    return render_template('index.tmpl')
-
+    return session['username']
 
 """
 Don't invoke this directly in production.  This is for development only.
