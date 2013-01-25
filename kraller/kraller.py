933e89ffd6a234379a52416dc201bc80f249b4d6
Josh Snyder <josh@code406.com>
2013-01-25 00:13:09 -0500

    Factor config into a file

--- kraller/kraller.py
+++ kraller/kraller.py
@@ -6,7 +6,6 @@ Kraller
 An application to allow signups for accounts on a server with a key.
 """
 
-from datetime import timedelta
 from functools import wraps
 import re
 from urllib import urlencode
@@ -19,11 +18,8 @@ from itsdangerous_session import ItsDangerousSessionInterface
 from user_management import create_user, add_ssh_key, try_getpwnam
 
 app = Flask(__name__)
-app.secret_key = 'hackme'
+app.config.from_envvar('KRALLER_SETTINGS')
 app.session_interface = ItsDangerousSessionInterface()
-app.permanent_session_lifetime = timedelta(hours=2)
-
-cas_server_endpoint = 'https://login.case.edu/cas/'
 
 username_re = "[a-z]{3}[0-9]*"
 gecos_re = "[A-Za-z0-9.' ]"
@@ -35,7 +31,7 @@ def my_cas_endpoint(redirect_to=None):
     return url_for('login', redirect_to=redirect_to, _external=True)
 
 def cas_login_url():
-    return cas_server_endpoint + 'login?' + urlencode(dict(service=my_cas_endpoint(), renew='true'))
+    return app.config['CAS_SERVER_ENDPOINT'] + 'login?' + urlencode(dict(service=my_cas_endpoint(), renew='true'))
 
 def requires_auth(f):
     @wraps(f)
@@ -51,7 +47,7 @@ def login():
     if 'ticket' in request.args and 'redirect_to' in request.args:
         ticket = request.args.get('ticket')
         redirect_to = request.args.get('redirect_to')
-        r = requests.get(cas_server_endpoint + 'validate', params=dict(service=my_cas_endpoint(redirect_to), ticket=ticket), verify=True)
+        r = requests.get(app.config['CAS_SERVER_ENDPOINT'] + 'validate', params=dict(service=my_cas_endpoint(redirect_to), ticket=ticket), verify=True)
         if not r.status_code == requests.codes.ok:
             # TODO: do we have logging of any sort?
             return abort(500)
